4215050db95830373487783ca96df46a06700a61
Maarten Balliauw <maarten@balliauw.be>
2013-04-30 16:44:39 +0200

    Initial commit
    Initial commit

---
+++ build.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal enableextensions
+
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Clean
+echo Cleaning...
+del /q src\NuPeek\bin\Release\*
+
+REM Build DotPeek 1.0 version
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\NuPeek.1.0.sln /p:Configuration="%config%" /t:Clean,Rebuild /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+mkdir install\NuPeek.1.0 2> NUL
+copy /y src\NuPeek\bin\Release\*.1.0.* install\NuPeek.1.0\
+copy /y src\NuPeek\bin\Release\NuGet.Core.* install\NuPeek.1.0\
+
+REM Clean
+echo Cleaning...
+del /q src\NuPeek\bin\Release\*
+
+REM Build DotPeek 1.1 version
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\NuPeek.1.1.sln /p:Configuration="%config%" /t:Clean,Rebuild /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+mkdir install\NuPeek.1.1 2> NUL
+copy /y src\NuPeek\bin\Release\*.1.1.* install\NuPeek.1.1\
+copy /y src\NuPeek\bin\Release\NuGet.Core.* install\NuPeek.1.1\
